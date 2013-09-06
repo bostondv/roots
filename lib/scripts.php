3ccd831370e19805d325a9d3db708e20b116322f
Boston Dell-Vandenberg <boston@pomelodesign.com>
2013-09-06 15:11:50 -0400

    removes custom css

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '44253bcd9e0a569046f102e7b19de390');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '432c135ef222956eb4626a287ef6906a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
