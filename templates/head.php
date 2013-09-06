9c76a4d4a1ac457fa6bc981cbcb1851acc4edba1
Boston Dell-Vandenberg <boston@pomelodesign.com>
2013-09-06 16:05:38 -0400

    Yoast SEO title

--- templates/head.php
+++ templates/head.php
@@ -2,7 +2,7 @@
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
   <meta charset="utf-8">
-  <title><?php wp_title('|', true, 'right'); ?></title>
+  <title><?php wp_title(''); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <?php wp_head(); ?>
