46122db1d530bef0f4515b7b720209c904f9649e
Boston Dell-Vandenberg <boston@pomelodesign.com>
2014-02-09 10:04:14 -0500

    fix missing closing tag, remove generated files

--- templates/head.php
+++ templates/head.php
@@ -2,7 +2,7 @@
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge"
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title(''); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
