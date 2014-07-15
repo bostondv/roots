d162d7ec23079d7c81d9f16d86efa7db47d1b2d8
Boston Dell-Vandenberg <boston@pomelodesign.com>
2014-07-15 17:43:24 -0400

    remove unecessary jquery enqueue (it's already enqueued by default)

--- lib/scripts.php
+++ lib/scripts.php
@@ -17,7 +17,6 @@ function roots_scripts() {
   }
 
   wp_register_script('roots_scripts', get_template_directory_uri() . '/build/js/main.js', array('jquery'), null, true);
-  wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
 }
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
