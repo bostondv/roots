1b9caba34497cb0066f742588391e891d82883e6
Boston Dell-Vandenberg <boston@pomelodesign.com>
2014-07-15 18:28:55 -0400

    move favicon and feed link above wp_head

--- templates/head.php
+++ templates/head.php
@@ -6,10 +6,9 @@
   <title><?php wp_title(''); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <?php wp_head(); ?>
-
   <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/build/img/favicon.png">
   <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/build/img/apple-touch-icon.png">
-  
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
+
+  <?php wp_head(); ?>
 </head>
